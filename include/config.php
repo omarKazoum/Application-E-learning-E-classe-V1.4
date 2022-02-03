0d0523b992fecef81899fa742436ae27a32fc01e
omar kazoum <omarKazoum96@gmail.com>
2022-02-03 09:40:02 +0100

    configured config for the remote hosting

--- include/config.php
+++ include/config.php
@@ -2,7 +2,7 @@
 /**
  * replace these values with your environment settings
  */
-$db_host_name='127.0.0.1';
-$db_user_name='root';
-$db_password='';
-$db_name='se_classe_db';
+$db_host_name='localhost';
+$db_user_name='id18301982_admin';//admin
+$db_password='oxxK#a5uVKEmIh=*';// oxxK#a5uVKEmIh=*
+$db_name='id18301982_se_classe_db';
