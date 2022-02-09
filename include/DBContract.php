e1d54a836bdfb9e693904088a38c1b0aa2e3615b
omar kazoum <omarKazoum96@gmail.com>
2022-02-09 20:55:19 +0100

    renamed a variable name to make it clean

--- include/DBContract.php
+++ include/DBContract.php
@@ -17,7 +17,7 @@ class DBContract
     public static string $Users_Col_Id='id';
     public static string $Users_Col_Email='email';
     public static string $Users_Col_UserName='name';
-    public static string $Users_Col_Password='pass';
+    public static string $Users_Col_PasswordHash='pass';
     //for payment_details table
     public static string $PaymentDetails_TableName='payments_details';
     public static string $PaymentDetails_Col_Id='id';
