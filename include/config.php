96b7f36eb91d8b162665867541da269213a6e33f
omar kazoum <omarKazoum96@gmail.com>
2022-02-09 20:53:49 +0100

    renamed database name to conform with the client specifications

--- include/config.php
+++ include/config.php
@@ -5,6 +5,6 @@
 $db_host_name='127.0.0.1';
 $db_user_name='root';//admin
 $db_password='';// oxxK#a5uVKEmIh=*
-$db_name='se_classe_db';
+$db_name='e_classe_db';
 $session_time_out_minutes=0.5;
 $PRODUCTION=false;
